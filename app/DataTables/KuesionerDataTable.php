291a7c3a4e2132e9d1f2aaac7d7e1fc9c49dba5e
Adiantha <utakrishar@gmail.com>
2022-04-24 13:38:25 +0700

    ubah format edited_at menjadi dd-mm-yy

--- app/DataTables/KuesionerDataTable.php
+++ app/DataTables/KuesionerDataTable.php
@@ -18,7 +18,13 @@ class KuesionerDataTable extends DataTable
     {
         $dataTable = new EloquentDataTable($query);
 
-        return $dataTable->addColumn('action', 'kuesioners.datatables_actions');
+        return $dataTable
+        ->addColumn('action', 'konfigurasis.datatables_actions')
+        ->editColumn('edited_at', function ($data) 
+        {   
+            if(isset($data->edited_at)) return date('d-m-Y', strtotime($data->edited_at) );
+            else return "";
+        });
     }
 
     /**
