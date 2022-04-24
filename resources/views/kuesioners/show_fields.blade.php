025b6bddbc0cde96b3e1e0a3f815b5a6f27c74f2
Adiantha <utakrishar@gmail.com>
2022-04-24 19:45:36 +0700

    create/update - hilangkan inputan edited by, created by, dan edited by

--- resources/views/kuesioners/show_fields.blade.php
+++ resources/views/kuesioners/show_fields.blade.php
@@ -1,24 +1,24 @@
 <!-- Jawaban Field -->
 <div class="col-sm-12">
     {!! Form::label('pertanyaan', 'pertanyaan:') !!}
-    <input type="text" name="nama" class="form-control" value="{{ $kuestioner->jawaban }}" readonly>
+    <input type="text" name="jawaban" class="form-control" value="@if(isset($kuesioner->jawaban)){{ $kuesioner->jawaban }}@endif" disabled>
 </div>
 
 <!-- Created By Field -->
 <div class="col-sm-12">
     {!! Form::label('created_by', 'Created By:') !!}
-    <input type="text" name="nama" class="form-control" value="{{ $kuestioner->created_by }}" readonly>
+    <input type="text" name="created_by" class="form-control" value="@if(isset($kuesioner->created_by)){{ $kuesioner->created_by }}@endif" disabled>
 </div>
 
 <!-- Edited By Field -->
 <div class="col-sm-12">
     {!! Form::label('edited_by', 'Edited By:') !!}
-    <input type="text" name="nama" class="form-control" value="{{ $kuestioner->edited_by }}" readonly>
+    <input type="text" name="edited_by" class="form-control" value="@if(isset($kuesioner->edited_by)){{ $kuesioner->edited_by }}@endif" disabled>
 </div>
 
 <!-- Edited At Field -->
 <div class="col-sm-12">
     {!! Form::label('edited_at', 'Edited At:') !!}
-    <input type="text" name="nama" class="form-control" value="{{ $kuestioner->edited_at }}" readonly>
+    <input type="text" name="edited_at" class="form-control" value="@if(isset($kuesioner->edited_at)){{ $kuesioner->edited_at }}@endif" disabled>
 </div>
 
