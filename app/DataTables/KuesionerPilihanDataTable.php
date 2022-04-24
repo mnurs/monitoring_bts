f2642443f63fb02baa6bc474e0a7f63697a50a58
Adiantha <utakrishar@gmail.com>
2022-04-25 00:17:05 +0700

    Update KuesionerPilihanDataTable.php

--- app/DataTables/KuesionerPilihanDataTable.php
+++ app/DataTables/KuesionerPilihanDataTable.php
@@ -76,7 +76,7 @@ class KuesionerPilihanDataTable extends DataTable
     protected function getColumns()
     {
         return [
-            'kuesioner' => ['name' => 'id_kuesioner', 'data' => 'id_kuesioner'],
+            'kuesioner' => ['name' => 'kuesioner', 'data' => 'kuesioner'],
             'pilihan_jawaban',
             'created_by',
             'edited_by',
