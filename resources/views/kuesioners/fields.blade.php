53c81b4f1d3f7c35ba628546c155e30e856064cc
Adiantha <utakrishar@gmail.com>
2022-05-27 09:31:58 +0700

    kuesioners = memperbaiki fungsi update dan create untuk created by dan edited by otomatis dari yang login

--- resources/views/kuesioners/fields.blade.php
+++ resources/views/kuesioners/fields.blade.php
@@ -13,13 +13,13 @@
 <!-- Created By Field -->
 <!-- <div class="form-group col-sm-6">
     {!! Form::label('created_by', 'Created By:') !!}
-    {!! Form::text('created_by', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255,'maxlength' => 255]) !!}
+    $input['created_by'] = $nameUser;
 </div> -->
 
 <!-- Edited By Field -->
 <!-- <div class="form-group col-sm-6">
     {!! Form::label('edited_by', 'Edited By:') !!}
-    {!! Form::text('edited_by', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255,'maxlength' => 255]) !!}
+    $input['edited_by'] = $nameUser;
 </div> -->
 
 <!-- Edited At Field -->
@@ -28,6 +28,44 @@
     {!! Form::text('edited_at', null, ['class' => 'form-control','id'=>'edited_at']) !!}
 </div>
  -->
+
+    public function store(CreateMonitoringRequest $request)
+    {
+        $input = $request->all();
+
+        $nameUser = $request->session()->get('name'); 
+        $input['created_by'] = $nameUser; 
+        $monitoring = $this->monitoringRepository->create($input);
+
+        Flash::success('Monitoring saved successfully.');
+
+        return redirect(route('monitorings.index'));
+    }
+
+    public function update($id, UpdateMonitoringRequest $request)
+    {
+        $input = $request->all();
+        $monitoring = $this->monitoringRepository->find($id);
+
+        if (empty($monitoring)) {
+            Flash::error('Monitoring not found');
+
+            return redirect(route('monitorings.index'));
+        }
+
+        $nameUser = $request->session()->get('name'); 
+        $now = new DateTime(); 
+        $input['edited_by'] = $nameUser;
+        $input['edited_at'] = $now;
+        $monitoring = $this->monitoringRepository->update($input, $id);
+
+        Flash::success('Monitoring updated successfully.');
+
+        return redirect(route('monitorings.index'));
+    }
+
+
+
 @push('page_scripts')
     <script type="text/javascript">
         $('#edited_at').datetimepicker({
