d1376ce023a52b0171537c14abe2993b449eca54
Belajar Git <92004207+Extra20Percent@users.noreply.github.com>
2022-06-10 18:23:32 +0700

    fix update password in dd

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -44,6 +44,6 @@ class ChangePasswordController extends Controller
    
         User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
    
-        dd('Password change successfully.');
+        return view('auth.login');
     }
 }
